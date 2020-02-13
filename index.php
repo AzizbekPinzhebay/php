257e6504004e629ab8e0a69f9f096eabbfa28fde
AzizbekPinzhebay <aziz_bek_ak@mail.ru>
2020-02-13 16:55:30 +0600

    13_1655

--- index.php
+++ index.php
@@ -93,13 +93,63 @@ echo 'Tsak 1. Создайте папку, добавьте в нее файлы
 
                     if($readWalpaper != '.' && $readWalpaper != '..'){
 
-                       echo"<div class='col-4 marginB'><img src='$wallpapers/$readWalpaper' alt='$readWalpaper'></div>" ;
+                       echo"<div class='col-4 marginB'><a href='$wallpapers/$readWalpaper' download><img src='$wallpapers/$readWalpaper' alt='$readWalpaper'></a></div>" ;
                     }
                 }
             }
+
         ?>
     </div>
+    <?php  echo "<hr>";?>
+
+       
             
+
+     <?php
+             echo 'Task 5. Создайте форму, которая принимает один параметр - имя папки, и по нажатию кнопки создает эту папку.<br>';
+
+             $newFolder = $_GET['folder_5'];
+
+             mkdir($newFolder);
+    ?>  
+
+        <form class="form-inline" action="index.php">
+
+            <div class="form-group mx-sm-3 mb-2">
+               <input type="text" class="form-control" id="creatFolder" name="folder_5">
+            </div>
+
+            <button type="submit" class="btn btn-primary mb-2">Enter name folder</button>
+        </form>
+
+
+
+
+
+        <?php
+             echo 'Task 6 .Создайте форму, которая принимает один параметр - имя папки, и по нажатию кнопки удаляет эту папку. Папки для удаления - пустые..<br>';
+
+             $deleteFolder = $_GET['deleteFolder_6'];
+
+             if(is_dir($deleteFolder)){
+
+                rmdir($deleteFolder);
+             }
+         ?>  
+
+        <form class="form-inline" action="index.php">
+
+            <div class="form-group mx-sm-3 mb-2">
+               <input type="text" class="form-control"  name="deleteFolder_6">
+            </div>
+
+            <button type="submit" class="btn btn-primary mb-2">Enter name folder</button>
+        </form>
+
+
+
+
+        
 
     </div>
 </body>
@@ -110,8 +160,8 @@ echo 'Tsak 1. Создайте папку, добавьте в нее файлы
 
 
 
-Создайте форму, которая принимает один параметр - имя папки, и по нажатию кнопки создает эту папку.
-Создайте форму, которая принимает один параметр - имя папки, и по нажатию кнопки удаляет эту папку. Папки для удаления - пустые.
+
+
 Создайте форму, которая принимает один параметр - имя файла, и после нажатия кнопки проверяет есть ли такой файл в папке.
 Создайте скрипт, который читает содежримое папки, и в зависимости от типа файла выводит имя файла, иконку, размер. Скрипт должен воспринимать файлы doc, docx, txt, jpg, jpeg, png. Иконки для сайта возьмите на iconfinder.com -->
 
